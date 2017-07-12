4dbbc0f6235800bf7a2c04afbfdad2a6462ca84c
Adam Binnersley <abinnersley@gmail.com>
2017-07-12 10:43:57 +0100

    Add database table layout SQL File

---
+++ files/database/database.sql
@@ -0,0 +1,25 @@
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blocked_ips`
+--
+
+CREATE TABLE IF NOT EXISTS `blocked_ips` (
+  `ip` varchar(45) NOT NULL,
+  PRIMARY KEY (`ip`),
+  UNIQUE KEY `ip` (`ip`)
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `blocked_ip_range`
+--
+
+CREATE TABLE IF NOT EXISTS `blocked_ip_range` (
+  `id` smallint(5) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `ip_start` varchar(45) NOT NULL,
+  `ip_end` varchar(45) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `unique_range` (`ip_start`,`ip_end`) USING BTREE
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
